33d0d4003d0dc5b6aa5c11267af04295f06d50df
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-11-23 01:27:16 +0600

    13-11-24

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,7 +41,8 @@ class AppServiceProvider extends ServiceProvider
             if (Schema::hasTable('categories')) {
                 View::share('categories', Category::active()->get());
             }
-            $randomNumber = rand(15, 30);
+            // $randomNumber = rand(15, 30);
+            $randomNumber = rand(10, 15);
             View::share('online', $randomNumber);
         } catch (Exception $e) {
             // Log the exception if needed
