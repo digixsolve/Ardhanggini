0195b8440b9a41ecd4cb80bdd1f12dc1fd1ec138
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2025-03-07 22:29:41 +0600

    23-02-25

--- resources/views/admin/pages/setting/partials/social_links.blade.php
+++ resources/views/admin/pages/setting/partials/social_links.blade.php
@@ -30,7 +30,8 @@
     </div>
     <div class="fv-row col-lg-4 mb-7">
 
-        <x-metronic.label class="fw-semibold fs-6 mb-2"><i class="fs-3 fa-brands fa-youtube pe-2"></i>Youtube
+        <x-metronic.label class="fw-semibold fs-6 mb-2">
+            <i class="fs-3 fa-brands fa-youtube pe-2"></i>Youtube
             URL</x-metronic.label>
         <x-metronic.input type="url" name="youtube_url" :value="optional($setting)->youtube_url"
             class="form-control form-control-solid mb-3 mb-lg-0" placeholder="Youtube URL" />
