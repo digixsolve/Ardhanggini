d3c3e3d14c68aa7f1276a2435124aada42e5656b
khandkershahed <khandkershahed23@gmail.com>
2025-10-04 02:16:53 +0600

    Checkout Success  Update

--- resources/views/frontend/pages/cart/checkoutSuccess.blade.php
+++ resources/views/frontend/pages/cart/checkoutSuccess.blade.php
@@ -37,12 +37,15 @@
             <div class="row">
                 <div class="col-lg-10 offset-lg-1 col-12">
                     <div class="p-0 p-lg-5 pt-lg-0">
-                        <div class="text-center">
-                            <h1>Thank You for Your Order!</h1>
-                            <p>আপনার অর্ডার সফলভাবে রেকর্ড করা হয়েছে, আমাদের টিম থেকে একজন প্রতিনিধি আপনাকে কল করে
-                                অর্ডারটি নিশ্চিত করার জন্য সহায়তা করবে। যেকোনো সময় সাহায্যের জন্য আমাদের সাথে যোগাযোগ
-                                করুন <a href="mailto:info.ardhanggini@gmail.com"
-                                    class="text-muted">info.ardhanggini@gmail.com</a> ।</p>
+                        <div class="text-center card">
+                            <div class="card-body p-lg-5 p-3">
+                                <h1>Thank You for Your Order!</h1>
+                                <p>আপনার অর্ডার সফলভাবে রেকর্ড করা হয়েছে, আমাদের টিম থেকে একজন প্রতিনিধি আপনাকে কল করে
+                                    অর্ডারটি নিশ্চিত করার জন্য সহায়তা করবে। যেকোনো সময় সাহায্যের জন্য আমাদের সাথে
+                                    যোগাযোগ
+                                    করুন <a href="mailto:info.ardhanggini@gmail.com"
+                                        class="text-info">info.ardhanggini@gmail.com</a> ।</p>
+                            </div>
                             {{-- <p>Your order has been successfully placed, and we’re preparing it for delivery.To keep a
                                 record, you can download
                                 your invoice using the button below. Need help? Contact our support team anytime at <a
